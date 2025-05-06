9f9dd14a71082fda6bd1ec73cc6241b695f5c199
rookie <piresj0318@gmail.com>
2025-05-06 18:14:32 +0900

    [FEAT] JPA STUDY 1주차

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,50 @@
 package hellojpa;
 
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import jakarta.transaction.Transactional;
+import org.hibernate.Hibernate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class JpaMain {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager(); // 쉽게 말해서
+        // 데이터베이스 커넥션을 받았다고 생각하면 됨
+        //code
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // 트랜잭션 시작
+
+        try {
+
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("hellojpa");
+
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+            tx.commit(); // 커밋
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
     }
+
+
 }
